e82709ee8a47ed47b8d7add6ba20c99d7ba791b3
Serhii Sierkov <Serhii Sierkov@DESKTOP-EOONJG7>
2020-11-19 19:37:59 -0500

    Add project files.

---
+++ FinalTest/Child.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalTest
+{
+    public class Child : Adult
+    {
+    }
+}
---
+++ FinalTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adult[] people = new Adult[2]
+            {
+                new Child
+                {
+                    FirstName = "Dante",
+                    LastName = "Sierkov"
+                },
+                new Adult
+                {
+                    FirstName = "Serhii",
+                    LastName = "Sierkov"
+                }
+            };
+            foreach (var human in people)
+            {
+                Console.WriteLine($"First Name: {human.FirstName}  Last Name: {human.LastName} Type of record: {human.GetType().Name}");
+            }
+        }
+    }
+}
